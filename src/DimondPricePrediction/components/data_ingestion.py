b1714b317aa41088737dae3c12261b85c6ada7cf
prabhat upadhyay <prabhatu786@gmial.com>
2024-08-08 12:51:41 +0530

    modified:exception.py

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import numpy as np
+from src.DimondPricePrediction.logger import Logging
+from src.DimondPricePrediction.exception import coustom
+from sklearn.model_selection import  train_test_split
+
+
 class Dataingestion:
     def __init__(self):
         pass
--- src/DimondPricePrediction/exception.py
+++ src/DimondPricePrediction/exception.py
@@ -17,11 +17,5 @@ class customexception(Exception):
         
     
 
-if __name__ == "__main__":
-    try:
-        
-        a=1/0
-    
-    except Exception as e:
-        raise customexception(e,sys)
-        
+
+ 
