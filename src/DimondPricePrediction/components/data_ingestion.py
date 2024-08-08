a498c1c1074dcf23a746ff2936ea51c03ae68ee5
prabhat upadhyay <prabhatu786@gmial.com>
2024-08-08 13:39:57 +0530

    inculed  __init.pt file in src folder

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-from src.DimondPricePrediction.logger import Logging
-from src.DimondPricePrediction.exception import coustom
 from sklearn.model_selection import  train_test_split
+from src.DimondPricePrediction.exception import customexception
+from src.DimondPricePrediction.logger import logging
+
 
 
 class Dataingestion:
@@ -11,4 +12,14 @@ class Dataingestion:
 
 
     def Intiated_data_ingestion(self):
-        pass    
+        logging.info("Intiated_data_ingestion")   
+        try:
+            data=pd.read_csv(path(os.path.join("notebook/data","train.csv")))
+            logging.info("Data load succefully")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train_test_split completed")
+
+
+        except:
+            pass 
--- src/DimondPricePrediction/exception.py
+++ src/DimondPricePrediction/exception.py
@@ -1,7 +1,5 @@
 
 import sys
-
-
 class customexception(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
