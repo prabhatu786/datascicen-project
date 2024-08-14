81c611f50023afb5f4d8334cd4f7d4f3f5d39256
prabhat upadhyay <prabhatu786@gmial.com>
2024-08-14 19:27:33 +0530

    commit

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,10 +1,9 @@
-
+import os
+import sys
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 #from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
-import os
-import sys
 from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import customexception
 import pandas as pd
